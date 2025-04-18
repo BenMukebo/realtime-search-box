22301f57727eee260fe29ff168a5282bb68d95f8
BenMukebo <l.benkasmukebo7@gmail.com>
2025-04-18 15:59:34 +0200

    Create SearchArticlesController and get_completed_searches for the testing

--- app/controllers/search_articles_controller.rb
+++ app/controllers/search_articles_controller.rb
@@ -1,4 +1,15 @@
 class SearchArticlesController < ApplicationController
   def index
+    @user = current_user
+
+    if @user
+      @user_searches = SearchArticle.get_completed_searches(@user.id)
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find_or_create_by_ip(request.remote_ip)
   end
 end
